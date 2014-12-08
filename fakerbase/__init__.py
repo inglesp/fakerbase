02d5b662458653ec20430b2d09ca111004be5312
Peter Inglesby <peter.inglesby@gmail.com>
2014-12-08 18:50:29 +0000

    Use relalg to for simple filtering

--- fakerbase/__init__.py
+++ fakerbase/__init__.py
@@ -1,20 +1,20 @@
-from collections import defaultdict
-
 from django.db.models import Manager, QuerySet
 from django.db.models.lookups import Exact
 from django.test import TestCase, TransactionTestCase
+
+from .relalg import Relation, union, eq, F
 
 
 assert len(Manager.__bases__) == 1
 OrigManagerBase = Manager.__bases__[0]
 
-
 orig_queryset_iterator = QuerySet.iterator
+
 
 def fakerbase_iterator(self):
     database = FakerbaseManager._FakerbaseManager__database
 
-    compiler = self.query.get_compiler(using=self.db) 
+    compiler = self.query.get_compiler(using=self.db)
     compiler.pre_sql_setup()
 
     assert len(self.query.tables) == 1
@@ -26,7 +26,7 @@ def fakerbase_iterator(self):
     assert len(join.join_cols) == 1
     assert join.join_type is None
 
-    records = database[join.table_name]
+    rel = database[join.table_name]
 
     if self.query.where.children:
         where = self.query.where
@@ -38,27 +38,24 @@ def fakerbase_iterator(self):
         fieldname = child.lhs.target.name
         value = child.rhs
 
-        records = [r for r in records if r[fieldname] == value]
+        p = eq(F(fieldname), value)
+        rel = rel.select(p)
 
-    fieldnames = [f.attname for f in self.model._meta.fields]
-    values_list = [[record[fn] for fn in fieldnames] for record in records]
-    return [self.model(**dict(zip(fieldnames, values))) for values in values_list]
-
+    return [self.model(**dict(zip(rel.attrs, t))) for t in rel.tuples]
 
 
 class FakerbaseManager(OrigManagerBase):
     __last_id = 0
-    __database = defaultdict(list)
+
+    @classmethod
+    def reset_database(cls):
+        cls.__database = {}
 
     def _insert(self, *args, **kwargs):
         if self.__manages_django_model():
             return super(FakerbaseManager, self)._insert(*args, **kwargs)
         else:
             return self.__insert(*args, **kwargs)
-
-    @classmethod
-    def reset_database(cls):
-        cls.__database = defaultdict(list)
 
     def __insert(self, objs, fields, return_id=False, raw=False, using=None):
         assert len(objs) == 1
@@ -67,11 +64,22 @@ class FakerbaseManager(OrigManagerBase):
 
         self.__last_id += 1
         obj = objs[0]
-        attrs = {field.attname: getattr(obj, field.attname) for field in fields}
-        attrs['id'] = self.__last_id
-        table_name = self.model._meta.db_table
-        self.__database[table_name].append(attrs)
+
+        attrs = ['id'] + [field.attname for field in fields]
+        t = [self.__last_id] + [getattr(obj, field.attname) for field in fields]
+
+        table_name = self.__table_name()
+
+        if table_name in self.__database:
+            rel = self.__database[table_name]
+        else:
+            rel = Relation(attrs, set())
+
+        self.__database[table_name] = union(rel, Relation(attrs, [t]))
         return self.__last_id
+
+    def __table_name(self):
+        return self.model._meta.db_table
 
     def __manages_django_model(self):
         return self.model.__module__.split('.')[0] == 'django'
@@ -93,5 +101,3 @@ class FakerbaseTestCase(TransactionTestCase):
 
 assert TestCase.__bases__ == (TransactionTestCase,)
 TestCase.__bases__ = (FakerbaseTestCase,)
-
-
